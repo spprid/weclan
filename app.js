d98d9a818309585de4112662ec7b47dfc4102707
hyangtech <spprid@naver.com>
2021-01-30 22:52:39 +0900

    레이아웃 변경

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var expressLayouts = require('express-ejs-layouts');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -22,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressLayouts);
 
 app.use('/', indexRouter);
 app.use('/clan', clanRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// 로그인
 router.get('/login', function(req, res, next) {
   res.render('users/login', { title: '로그인' });
 });
 
+// 로그아웃
 router.get('/logout', function(req, res, next){
   res.redirect('/');
+});
+
+// 회원가입
+router.get('/join', (req, res, next) => {
+  res.render('users/join', { title: '회원가입' });
 });
 
 module.exports = router;
